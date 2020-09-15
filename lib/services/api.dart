a0425a268a085a15297a512ec6eeaaa44d062f54
Kyle Coralejo <kylecjo@gmail.com>
2020-09-15 17:54:26 -0600

    just formatting

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'api_keys.dart';
 
-class API{
+class API {
   API({@required this.apiKey});
   final String apiKey;
 
@@ -13,9 +13,14 @@ class API{
   //   host, '/search', queries
   // );
 
-  Uri searchUri({double lat, double long}){
-    Uri uri = Uri.https(host, '/v3/businesses/search', { 'latitude': lat.toString(), 'longitude' : long.toString(), 'radius' : '2000', 'limit' : '20', 'categories' : 'restaurants'  });
-    return uri;  
+  Uri searchUri({double lat, double long}) {
+    Uri uri = Uri.https(host, '/v3/businesses/search', {
+      'latitude': lat.toString(),
+      'longitude': long.toString(),
+      'radius': '2000',
+      'limit': '30',
+      'categories': 'restaurants'
+    });
+    return uri;
   }
-
 }
