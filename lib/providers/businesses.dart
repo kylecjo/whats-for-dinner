adfc3921bca0d588d63295f692c128a48b148c2d
Kyle Coralejo <kylecjo@gmail.com>
2020-09-24 01:25:56 -0600

    added business provider

---
+++ lib/providers/businesses.dart
@@ -0,0 +1,46 @@
+import '../models/business.dart';
+import 'package:flutter/foundation.dart';
+
+
+class Businesses with ChangeNotifier{ 
+  List<Business> _businesses = [];
+  List<Business> _hidden = [];
+  List<Business> _favorites = [];
+
+  List<Business> get businesses {
+    return [..._businesses];
+  }
+
+  List<Business> get hidden {
+    return [..._hidden];
+  }
+
+  List<Business> get favorites {
+    return [..._favorites];
+  }
+
+  void initBusinesses(List<Business> businesses){
+    _businesses = businesses;
+    notifyListeners();
+  }
+
+  void addHidden(Business business){
+    _hidden.add(business);
+    notifyListeners();  
+  }
+
+  void addFavorite(Business business){
+    _favorites.add(business);
+    notifyListeners();
+  }
+
+  void removeBusiness(Business businessToRemove){
+    _businesses.removeWhere((business) => business == businessToRemove);
+    notifyListeners();
+  }
+
+  Business findById(String id){
+    return _businesses.where((business) => business.id == id) as Business;
+  }
+
+}
