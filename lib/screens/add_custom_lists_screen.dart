2d6af3dc85ea5f3d8f0df9eaaa8f8682d7d7c98c
Kyle Coralejo <kylecjo@gmail.com>
2020-10-21 23:16:43 -0600

    refactored new list button into its own widget

--- lib/screens/add_custom_lists_screen.dart
+++ lib/screens/add_custom_lists_screen.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import '../providers/auth.dart';
 import '../providers/custom_lists.dart';
 import '../screens/custom_list_screen.dart';
+import '../widgets/add_custom_list_button.dart';
 import '../widgets/custom_list_tile.dart';
 import '../widgets/favorite_list_tile.dart';
 
@@ -37,56 +38,7 @@ class AddCustomListsScreen extends StatelessWidget {
                   return Column(
                       crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                              vertical: 5, horizontal: 15),
-                          child: RaisedButton(
-                              color: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(18.0),
-                                side: BorderSide(color: Colors.teal),
-                              ),
-                              child: Text('+ New List',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .headline3
-                                      .copyWith(
-                                          fontSize: 13,
-                                          color: Colors.teal,
-                                          fontWeight: FontWeight.bold)),
-                              onPressed: () {
-                                return showDialog(
-                                    context: context,
-                                    builder: (context) {
-                                      return AlertDialog(
-                                        title: Text('Enter list name',
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .bodyText2
-                                                .copyWith(fontSize: 15)),
-                                        content: TextField(
-                                          controller: textController,
-                                          textInputAction: TextInputAction.go,
-                                        ),
-                                        actions: <Widget>[
-                                          new FlatButton(
-                                            child: new Text('Submit'),
-                                            onPressed: () {
-                                              Provider.of<CustomLists>(context,
-                                                      listen: false)
-                                                  .addCustomList(
-                                                      authProvider.uid,
-                                                      authProvider.email,
-                                                      textController.text);
-                                              textController.clear();
-                                              Navigator.of(context).pop();
-                                            },
-                                          )
-                                        ],
-                                      );
-                                    });
-                              }),
-                        ),
+                        AddCustomListButton(textController),
                         FavoriteListTile(),
                         Expanded(
                           child: Consumer<CustomLists>(
---
+++ lib/widgets/add_custom_list_button.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/auth.dart';
+import '../providers/custom_lists.dart';
+
+class AddCustomListButton extends StatelessWidget {
+  final textController;
+
+  AddCustomListButton(this.textController);
+
+  @override
+  Widget build(BuildContext context) {
+    final authProvider = Provider.of<Auth>(context);
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+      child: RaisedButton(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(18.0),
+            side: BorderSide(color: Colors.teal),
+          ),
+          child: Text('+ New List',
+              style: Theme.of(context).textTheme.headline3.copyWith(
+                  fontSize: 13,
+                  color: Colors.teal,
+                  fontWeight: FontWeight.bold)),
+          onPressed: () {
+            return showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text('Enter list name',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText2
+                            .copyWith(fontSize: 15)),
+                    content: TextField(
+                      controller: textController,
+                      textInputAction: TextInputAction.go,
+                    ),
+                    actions: <Widget>[
+                      new FlatButton(
+                        child: new Text('Submit'),
+                        onPressed: () {
+                          Provider.of<CustomLists>(context, listen: false)
+                              .addCustomList(authProvider.uid,
+                                  authProvider.email, textController.text);
+                          textController.clear();
+                          Navigator.of(context).pop();
+                        },
+                      )
+                    ],
+                  );
+                });
+          }),
+    );
+  }
+}
